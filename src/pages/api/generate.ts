31b6ad99367c071d28946a631d85384b41d99e2b
Rocky Zhang <rockyzh@gmail.com>
2023-03-21 22:41:47 +0800

    revert

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -5,7 +5,7 @@ import { generatePayload, parseOpenAIStream } from '@/utils/openAI'
 import { verifySignature } from '@/utils/auth'
 import type { APIRoute } from 'astro'
 
-const apiKey = import.meta.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY
+const apiKey = import.meta.env.OPENAI_API_KEY
 const httpsProxy = import.meta.env.HTTPS_PROXY
 const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/, '')
 const sitePassword = import.meta.env.SITE_PASSWORD
